89f851d8393c425c8e8179fcac2641738e84b77d
nithinvmavoor <nithinvmavoor@gmail.com>
2026-01-22 05:42:27 +0000

    deployment issue allowed hosts.

--- frontend-web/vite.config.ts
+++ frontend-web/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,               // listen on 0.0.0.0
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      "ec2-13-200-229-186.ap-south-1.compute.amazonaws.com", // TODO: Make this through env variable
+    ],
+  },
 })
