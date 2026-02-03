aab4c6ee7e7ad917cf89827ceea859aa309b571c
nithinvmavoor <nithinvmavoor@gmail.com>
2026-02-03 11:03:36 +0000

    TESTING CICD

--- services/auth-service/src/app.ts
+++ services/auth-service/src/app.ts
@@ -41,7 +41,8 @@ app.use(
 
 app.use("/auth/health", healthRoutes);
 app.use("/auth/", authRoutes); // /register & /login
-console.log("+++++++++++++.............TESTING CICD......+++++++++++");
+console.log("Added minor change");
+console.log("++++++++++++.......TESTING CICD......+++++++++++");
 
 app.use((req, res) => {
   sendErrorResponse(res, 404, "Page not found", "NOT_FOUND");
